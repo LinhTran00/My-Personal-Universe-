b388b1f69d3800640d7fc05b919fca1a77fc3eea
Linh Tran <linhtrant433@gmail.com>
2025-11-09 01:35:28 -0600

    add style to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function Home() {
     return (
         <div>
             <h1>Welcome to Linh's Universe</h1>
             <p>Select a section to explore</p>
-            <Link to="/about">About Me</Link>
-            <Link to="/hobbies">Hobbies</Link>
-            <Link to="/favorites">Favorites</Link>
+            <div className="flex-container">
+                <div className="box"><Link to="/about">About Me</Link></div>
+                <div className="box"><Link to="/hobbies">Hobbies</Link></div>
+                <div className="box"><Link to="/favorites">Favorites</Link></div>
+            </div>
+            
         </div>
     );
 }
