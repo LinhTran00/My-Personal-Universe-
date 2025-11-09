0ca7e678ffab6354664d7d4d11343e27b2166b91
Linh Tran <linhtrant433@gmail.com>
2025-11-09 01:00:32 -0600

    add them color palette for home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,10 @@ function Home() {
         <div>
             <h1>Welcome to Linh's Universe</h1>
             <p>Select a section to explore</p>
-            <ul>
-                <li><Link to="/about">About Me</Link></li>
-                <li><Link to="/hobbies">Hobbies</Link></li>
-                <li><Link to="/favorites">Favorites</Link></li>
-            </ul>
+            <Link to="/about">About Me</Link>
+            <Link to="/hobbies">Hobbies</Link>
+            <Link to="/favorites">Favorites</Link>
         </div>
-
     );
 }
 
